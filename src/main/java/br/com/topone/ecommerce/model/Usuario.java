069e0749898cb34d5bf7c8e92528105593e11632
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-01-31 21:01:21 -0300

    Criação dos Enums e classe usuario

---
+++ src/main/java/br/com/topone/ecommerce/model/Usuario.java
@@ -0,0 +1,80 @@
+package br.com.topone.ecommerce.model;
+
+import javax.persistence.*;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "usuario")
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+    private Long id;
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dataAtualSenha;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pesssoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,
+            name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "usuario_acesso", uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"},
+            name = "unique_acesso_user"), joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id",
+            table = "usuario", unique = false, foreignKey = @ForeignKey(name = "usuario_fk",
+            value = ConstraintMode.CONSTRAINT)),
+            inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false,
+            referencedColumnName = "id", table = "acesso",
+            foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+    private List<Acesso> acessos;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { /*ACESSOS*/
+        return this.acessos;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
