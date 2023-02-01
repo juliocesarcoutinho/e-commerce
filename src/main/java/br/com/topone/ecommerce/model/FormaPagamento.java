4cf4afcb4dd79b104b5f8aff280538507936c006
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-01-31 21:43:22 -0300

    Criação de entidade, Enums, e arquivos para o versionador de codigo que futuramente irei implantar no projeto.

---
+++ src/main/java/br/com/topone/ecommerce/model/FormaPagamento.java
@@ -0,0 +1,38 @@
+package br.com.topone.ecommerce.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+    @Entity
+    @Getter
+    @Setter
+    @Table(name = "forma_pagamento")
+    @SequenceGenerator(name = "seq_forma_pagamento", sequenceName = "seq_forma_pagamento", allocationSize = 1,
+            initialValue = 1)
+    public class FormaPagamento implements Serializable {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_pagamento")
+        private Long Id;
+
+        @Column(nullable = false)
+        private String descricao;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            FormaPagamento that = (FormaPagamento) o;
+
+            return Id.equals(that.Id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Id.hashCode();
+        }
+    }
