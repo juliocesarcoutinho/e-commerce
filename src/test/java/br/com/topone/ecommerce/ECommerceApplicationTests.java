40ba569deb8212f5e6831e79772d605ff00132b7
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-02-06 20:01:04 -0300

    testes unitarios

--- src/test/java/br/com/topone/ecommerce/ECommerceApplicationTests.java
+++ src/test/java/br/com/topone/ecommerce/ECommerceApplicationTests.java
@@ -8,12 +8,18 @@ import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @SpringBootTest(classes = ECommerceApplication.class)
 class ECommerceApplicationTests extends TestCase {
 
     @Autowired
     private AcessoController acessoController;
+
+    @Autowired
+    private AcessoRepository acessoRepository;
 
     @Test
     public void testCadastroAcesso() {
@@ -28,6 +34,28 @@ class ECommerceApplicationTests extends TestCase {
 
         /*Verifca se veio o que foi gravado*/
         assertEquals("ROLE_DIRETOR", acesso.getDescricao());
+
+        //Teste de Carregamento testando os repositorios
+        Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+
+        assertEquals(acesso.getId(), acesso2.getId());
+
+        //Teste de delete da camada de persistencia
+        acessoRepository.deleteById(acesso2.getId());
+        acessoRepository.flush(); /*Roda o sql no banco*/
+        Acesso acesso3 = acessoRepository.findById(acesso.getId()).orElse(null);
+
+        assertEquals(true, acesso3 == null);
+
+        /*Teste de query*/
+        acesso =new Acesso();
+        acesso.setDescricao("ROLE_TESTE");
+        acesso = acessoController.salvarAcesso(acesso).getBody();
+
+        List<Acesso> acessos = acessoRepository.buscarAcessoDesc("TESTE".trim().toUpperCase());
+
+        assertEquals(1, acessos.size());
+        acessoRepository.deleteById(acesso.getId());
 
     }
 
