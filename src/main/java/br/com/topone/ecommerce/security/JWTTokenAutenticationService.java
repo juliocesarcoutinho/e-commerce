343ec412c97edc1ded7eb2e3627221f3bb15ad40
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-02-09 21:51:43 -0300

    Configuração do token JWT

--- src/main/java/br/com/topone/ecommerce/security/JWTTokenAutenticationService.java
+++ src/main/java/br/com/topone/ecommerce/security/JWTTokenAutenticationService.java
@@ -2,9 +2,11 @@ package br.com.topone.ecommerce.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
@@ -34,10 +36,44 @@ public class JWTTokenAutenticationService {
 
         /* Da a resposta para a tela */
         response.addHeader(HEADER_STRING, token);
+        liberacaoCors(response);
 
         /*Usado para teste no Postman*/
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
 
+    }
+
+    /*Metodo que retorna o usuario validado pelo token caso não seja validado retorna null*/
+    public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response){
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null){
+            String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+
+            /*Faz a Liberação do token e obtem o usuario*/
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(tokenLimpo)
+                    .getBody().getSubject();
+        }
+        liberacaoCors(response);
+        return null;
+    }
+
+    /*Correção de erro de Cors nos navegadores*/
+    private void liberacaoCors(HttpServletResponse response){
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 
 }
