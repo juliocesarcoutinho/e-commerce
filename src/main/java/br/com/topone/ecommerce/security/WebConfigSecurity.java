2def56fca771acd6b317d891e97bbdcfe191385b
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-02-13 21:27:56 -0300

    Configuração do token JWT

--- src/main/java/br/com/topone/ecommerce/security/WebConfigSecurity.java
+++ src/main/java/br/com/topone/ecommerce/security/WebConfigSecurity.java
@@ -52,7 +52,7 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso") /*Ignora Url */
-                .antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+//        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso") /*Ignora Url */
+//                .antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
     }
 }
